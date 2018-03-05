19e1b186d11f4520c659eacd5fb7e3de9c488a91
Gabriel Martinez <jei.sum41@gmail.com>
2018-03-05 18:36:20 -0500

    packaje.json updated

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 var mongoose = require("mongoose");
 
 var Conductor = require("./../models/conductor");
+var Flota = require("./../models/flota");
+
 const app = express();
 app.use(morgan("combined"));
 app.use(bodyParser.json());
@@ -88,5 +90,71 @@ app.delete("/conductores/:id", (req, res) => {
     }
   );
 });
+
+
+
+//Flotas
+app.get("/flotas", (req, res) => {
+  Flota.find({}, function(error, flotas) {
+    if (error) {
+      console.error(error);
+    }
+    res.send({
+      flotas: flotas
+    });
+  }).sort({ _id: -1 });
+});
+
+app.get("/flotas/:id", (req, res) => {
+  Flota.find({ _id: req.params.id }, function(error, flota) {
+    if (error) {
+      console.error(error);
+    }
+    res.send({
+      flota: flota
+    });
+  }).sort({ _id: -1 });
+});
+
+app.post("/flotas", (req, res) => {
+  var db = req.db;
+  var Placa = req.body.Placa;
+  var Ciudad = req.body.Ciudad;
+  var Modelo = req.body.Modelo;
+ 
+
+  var new_flota = new Flota({
+    Placa: Placa,
+    Ciudad: Ciudad,
+    Modelo: Modelo
+  });
+
+  new_flota.save(function(error) {
+    if (error) {
+      console.log(error);
+    }
+    res.send({
+      success: true,
+      message: "Flota guardada con exito!"
+    });
+  });
+});
+
+app.delete("/flotas/:id", (req, res) => {
+  var db = req.db;
+  console.log(req.params.id);
+  Flota.remove(
+    {
+      _id: req.params.id
+    },
+    function(err, post) {
+      if (err) res.send(err);
+      res.send({
+        success: true
+      });
+    }
+  );
+});
+
 
 app.listen(process.env.PORT || 8081);
